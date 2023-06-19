63e2fb52c0531c1dff86b722d496e18072cfdbc9
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2023-06-20 01:39:48 +0600

    folders

--- app/src/main/java/com/example/filemanagerapplication/MyAdapter.java
+++ app/src/main/java/com/example/filemanagerapplication/MyAdapter.java
@@ -1,8 +1,11 @@
 package com.example.filemanagerapplication;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -22,22 +25,38 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        View view= LayoutInflater.from(context).inflate(R.layout.recyler_item,parent,false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
+        File selectedFile=filesAndFolders[position];
+        holder.textView.setText(selectedFile.getName());
+        if(selectedFile.isDirectory()){
+            holder.imageView.setImageResource(R.drawable.baseline_folder);
+        }else{
+            holder.imageView.setImageResource(R.drawable.baseline_insert_drive_file);
+        }
+
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return filesAndFolders.length;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView textView;
+        ImageView imageView;
+
         public ViewHolder(View itemView){
             super(itemView);
+            textView=itemView.findViewById(R.id.file_name_text_view);
+            imageView=itemView.findViewById(R.id.icon_view);
         }
     }
 }
