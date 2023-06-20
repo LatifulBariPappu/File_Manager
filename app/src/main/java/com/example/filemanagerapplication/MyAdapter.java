b0dbe35fa8380e14d80f583a7e3c2dc0a261befb
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2023-06-20 17:40:06 +0600

    showed inside files

--- app/src/main/java/com/example/filemanagerapplication/MyAdapter.java
+++ app/src/main/java/com/example/filemanagerapplication/MyAdapter.java
@@ -1,11 +1,17 @@
 package com.example.filemanagerapplication;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -40,6 +46,66 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
         }else{
             holder.imageView.setImageResource(R.drawable.baseline_insert_drive_file);
         }
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(selectedFile.isDirectory()){
+                    Intent intent=new Intent(context,FileListActivity.class);
+                    String path= selectedFile.getAbsolutePath();
+                    intent.putExtra("path",path);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    context.startActivity(intent);
+                }else{
+                    //open the file
+                    try {
+                        Intent intent=new Intent();
+                        intent.setAction(Intent.ACTION_VIEW);
+                        String type="image/*";
+                        intent.setDataAndType(Uri.parse(selectedFile.getAbsolutePath()),type);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        context.startActivity(intent);
+                    } catch (Exception e) {
+                        Toast.makeText(context.getApplicationContext(),"Cannot open the file", Toast.LENGTH_SHORT).show();;
+                    }
+                }
+            }
+        });
+
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                PopupMenu popupMenu=new PopupMenu(context,v);
+                popupMenu.getMenu().add("DELETE");
+                popupMenu.getMenu().add("MOVE");
+                popupMenu.getMenu().add("RENAME");
+
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        if (item.getTitle().equals("DELETE")){
+                            boolean deleted=selectedFile.delete();
+                            if (deleted){
+                                Toast.makeText(context.getApplicationContext(), "DELETE", Toast.LENGTH_SHORT).show();
+                                v.setVisibility(View.GONE);
+                            }
+
+                        }
+                        if (item.getTitle().equals("MOVE")){
+                            Toast.makeText(context.getApplicationContext(), "MOVED", Toast.LENGTH_SHORT).show();
+
+                        }
+                        if (item.getTitle().equals("RENAME")){
+                            Toast.makeText(context.getApplicationContext(), "RENAMED", Toast.LENGTH_SHORT).show();
+
+                        }
+                        return true;
+                    }
+                });
+                popupMenu.show();
+                return true;
+            }
+        });
 
     }
 
